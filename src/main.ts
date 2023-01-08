24c02f42042af5982cae5ea279a41a2b37fabccc
Mike Koss <mike@mckoss.com>
2023-01-08 15:24:16 -0800

    Add TweakPane and material color

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { Pane } from 'tweakpane';
+
 export { init };
 
 let WINDOW_WIDTH = 800;
 let WINDOW_HEIGHT = 800;
+
+// Tweak Pane Options
+const tweakParams = {
+    color: 0xff0000,
+};
 
 function init() {
     console.log('Application starting...');
@@ -18,14 +25,27 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshStandardMaterial({ color: 'white' });
+    const material = new THREE.MeshStandardMaterial({ color: tweakParams.color });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    const light = new THREE.HemisphereLight( 0xe0e0ff, 0x080820, 1 );
+    const light = new THREE.HemisphereLight(0xffffff, 0x202020, 1);
     scene.add( light );
 
     camera.position.z = 5;
+
+    const paneElement = document.createElement('div');
+    paneElement.className = 'tweak-pane';
+    document.body.appendChild(paneElement);
+
+    const pane = new Pane({
+        title: '3D Maze Options',
+        container: paneElement
+    });
+
+    pane.addInput(tweakParams, 'color', { view: 'color' }).on('change', (ev) => {
+        material.color.set(tweakParams.color);
+    });
 
     function animate() {
         requestAnimationFrame( animate );
